ef4404621cc0dee4af6e92c1974419e7fb9e8cd9
xianglei <horseman@163.com>
2012-02-06 19:28:47 +0800

    qq

--- download.php
+++ download.php
@@ -6,17 +6,9 @@ if(!@$_GET['filename'])
 	die('invalid url');
 }
 
-if(file_exists(@$_GET['filename']))
-{
 	$filename = explode('/',$_GET['filename']);
 	header("Content-Type: application/force-download");
 	header("Content-Disposition: attachment; filename=".$filename[1]);
 	readfile($_GET['filename']);
 
-	//unlink($_GET['filename']);
-}
-else
-{
-	die('Invalid Filename');
-}
 ?>
