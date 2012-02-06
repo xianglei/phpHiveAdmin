3d40492d40cae615a6e800b30ff67940a5e3fe73
xianglei <horseman@163.com>
2012-02-06 10:05:49 +0800

    modify frame margin
    Signed-off-by: xianglei <horseman@163.com>

--- getThriftSchema.php
+++ getThriftSchema.php
@@ -9,8 +9,8 @@ $client->execute('add jar '.$env['hive_jar']);
 $status = $client->getThriftSchema();
 var_dump($status);
 
-//echo 'Field Schemas: '.$status->fieldSchemas.'<br />';
-//echo 'Properties: '.$status->properties.'<br />';
+echo 'Field Schemas: '.$status->fieldSchemas.'<br />';
+echo 'Properties: '.$status->properties.'<br />';
 
 $transport->close();
 
