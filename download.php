80580a66909553c6b8211d9087952bb2694e0dc0
xianglei <horseman@163.com>
2012-02-06 19:38:55 +0800

    dsdd

--- download.php
+++ download.php
@@ -6,11 +6,17 @@ if(!@$_GET['filename'])
 	die('invalid url');
 }
 
-	/*$filename = explode('/',$_GET['filename']);
+if(file_exists(@$_GET['filename']))
+{
+	$filename = explode('/',$_GET['filename']);
 	header("Content-Type: application/force-download");
 	header("Content-Disposition: attachment; filename=".$filename[1]);
-	readfile($_GET['filename']);*/
-	header('location:'.$_GET['filename']);
-	
+	readfile($_GET['filename']);
 
+	//unlink($_GET['filename']);
+}
+else
+{
+	die('Invalid Filename');
+}
 ?>
