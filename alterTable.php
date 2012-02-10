48f8b80fad9bb5df7570e4a488b73081e9b57221
xianglei <horseman@163.com>
2012-02-10 12:27:50 +0800

    debug

--- alterTable.php
+++ alterTable.php
@@ -111,7 +111,7 @@ else
 				echo "<input type=hidden name=confirm value=1><br>";
 				echo "Sure???<br>";
 				echo "<input type=submit name=submit value=".$lang['submit'].">";
-				echo "<input type=button name=cancel value=".$lang['cancel']." onclick=\"javascript:this.location=index.php?frame=right\">";
+				echo "<input type=button name=cancel value=".$lang['cancel']." onclick=\"javascript:this.location.href=index.php?frame=right\">";
 				echo "</form>";
 			}
 			else
@@ -119,8 +119,8 @@ else
 				foreach ($_POST['sql'] as $k => $v)
 				{
 					echo $client->execute($v);
-					echo "<script>window.alert('success');this.location=index.php?frame=right;</script>";
 				}
+				echo "<script>window.alert('success');this.location.href=index.php?frame=right;</script>";
 			}
 		}
 	}
