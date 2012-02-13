61fe9029d92c03d0ae832d91a51fbcc0be93ff4c
xianglei <horseman@163.com>
2012-02-13 16:51:35 +0800

    aa

--- fileBrowser.php
+++ fileBrowser.php
@@ -2,13 +2,35 @@
 include_once 'config.inc.php';
 include_once 'templates/style.css';
 
-$dir = $env['hdfsToHiveDir'];
+if(!@$_GET['dir'])
+{
+	$dir = $env['hdfsToHiveDir'];
+}
+else
+{
+	$dir = $_GET['dir'];
+}
+
 if (is_dir($dir)) {
     if ($dh = opendir($dir)) {
-        while (($file = readdir($dh)) !== false) {
-            echo "filename: $file : filetype: " . filetype($dir . $file) . "<br>\n";
+		echo "<table border=1>";
+        while (($file = readdir($dh)) !== false)
+		{
+			echo "<tr>";
+			if(($file == '.') || ($file == '..'))
+			{
+				continue;
+			}
+			else
+			{
+				echo "<td>filename: <a href=fileBrowser.php?dir=".$env['hdfsToHiveDir'].$file.">$file </td>\n";
+				echo "<td>filetype: ".filetype($dir.$file)."</td>\n";
+				echo "<td>filesize: ".filesize($dir.$file)."</td>\n";
+			}
+			echo "</tr>";
         }
         closedir($dh);
+		echo "</table>";
     }
 }
 
