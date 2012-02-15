da4ce3e84d2705c296adf8df7ac576458e0ce037
xianglei <horseman@163.com>
2012-02-15 13:28:58 +0800

    debug

--- alterTable.php
+++ alterTable.php
@@ -54,10 +54,10 @@ else
 				{
 					$color = "bgcolor=\"#99FFFF\"";
 				}
-				//echo '<tr '.$color.'>';
+				echo '<tr '.$color.'>';
 				foreach($value as $k => $v)
 				{
-					echo '<tr '.$color.'>';
+					//echo '<tr '.$color.'>';
 					//echo '<td>';
 					if($key == 'type')
 					{
@@ -88,9 +88,9 @@ else
 					}
 					//echo '</td>';
 					$i++;
-					echo '</tr>';
+					//echo '</tr>';
 				}
-				//echo '<tr>';
+				echo '</tr>';
 			}
 			$i = 0;
 			echo '</table><br>';
