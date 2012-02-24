d140e0ac32bec184a63738c851f58c6947e00f90
xianglei <horseman@163.com>
2012-02-24 11:12:54 +0800

    dropEtl.php

---
+++ dropEtl.php
@@ -0,0 +1,14 @@
+﻿<?php
+include_once "templates/style.css";
+include_once "config.inc.php";
+
+if(!@$_GET['filename'])
+{
+	die($lang['noFileChoose']);
+}
+else
+{
+	unlink("./etl/".$_GET['filename']);
+	echo "<script>alert(".$lang['success'].");window.location='execEtl.php'";
+}
+?>
