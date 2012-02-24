83ca669ce2d2145a5f87f790055486df404f43d2
xianglei <horseman@163.com>
2012-02-24 11:13:31 +0800

    a

--- dropEtl.php
+++ dropEtl.php
@@ -8,7 +8,13 @@ if(!@$_GET['filename'])
 }
 else
 {
-	unlink("./etl/".$_GET['filename']);
-	echo "<script>alert(".$lang['success'].");window.location='execEtl.php'";
+	if(unlink("./etl/".$_GET['filename']))
+	{
+		echo "<script>alert(".$lang['success'].");window.location='execEtl.php'";
+	}
+	else
+	{
+		die "Unknown error";
+	}
 }
 ?>
