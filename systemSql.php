37aee0c2fdbf13cbcc7fb9a2af6ea2acb0d2f0df
xianglei <horseman@163.com>
2012-02-29 13:10:34 +0800

    env set

--- systemSql.php
+++ systemSql.php
@@ -1,9 +1,14 @@
 <?php
 set_time_limit(0);
-$env['hive'] = "/opt/modules/hive/hive-0.7.1/bin/hive -e";
+$env['hive'] = "/opt/modules/hive/hive-0.7.1";
+$env['hadoop'] = "/opt/modules/hadoop/hadoop-0.20.203.0";
+$env['java'] = "/usr/java/jdk1.6.0_21";
 $sql = "select * from asf.asf_20120224 limit 10";
 
 $exec = $env['hive']." \"".$sql."\"";
 echo $exec;
-echo exec($exec);
+echo exec("export HADOOP_HOME=".$env['hadoop']);
+echo exec("export JAVA_HOME=".$env['java']);
+echo exec("export HIVE_HOME=".$env['hive']);
+echo exec($env['hive']."/bin/hive -e \"".$sql."\"");
 ?>
