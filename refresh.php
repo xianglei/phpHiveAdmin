4d226db7513413a0d8f9f4a9bf34eb0e1c423873
xianglei <horseman@163.com>
2012-03-08 19:17:45 +0800

    reverse stderr output

--- refresh.php
+++ refresh.php
@@ -11,15 +11,20 @@ else
 	$str = $_GET['str'];
 	if(file_exists("/tmp/hive_run.".$str.".out"))
 	{
-		$fp = @fopen("/tmp/hive_run.".$str.".out","r");
+		/*$fp = @fopen("/tmp/hive_run.".$str.".out","r");
 		while(!@feof($fp))
 		{
 			$str .= fread($fp,128);
 		}
 		echo nl2br($str);
-		@fclose($fp);
+		@fclose($fp);*/
 		
-		echo "<br><br>";
+		$array = @file("/tmp/hive_run.".$str.".out");
+		$array = array_reverse($array);
+		foreach($array as $k=>$v)
+		{
+			echo nl2br($v);
+		}
 	}
 	else
 	{
