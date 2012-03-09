d635339d32a11955a8361f1f253d6baf6d36f82c
xianglei <horseman@163.com>
2012-03-09 09:57:19 +0800

    a

--- getResult.php
+++ getResult.php
@@ -9,7 +9,7 @@ if(!@$_GET['str'])
 else
 {
 	$str = @$_GET['str'];
-	if(file_exists("/tmp/hive_res.".$str.".out"))
+	if(file_exists("/tmp/hive_res.".$str.".out") || filesize("/tmp/hive_res.".$str.".out") != 0)
 	{
 		$fp = fopen("/tmp/hive_res.".$str.".out","r");
 		$i = 0;
@@ -19,10 +19,31 @@ else
 			$i++;
 		}
 		$array = explode("\n",$string);
+		$i = 0;
+		echo "<table border=1 cellspacing=1 cellpadding=3>\n";
 		foreach($array as $k=>$v)
 		{
-			echo $v."<br>";
+			if(($i % 2) == 0)
+			{
+				$color = "bgcolor=\"".$env['trColor1']."\"";
+			}
+			else
+			{
+				$color = "bgcolor=\"".$env['trColor2']."\"";
+			}
+			echo "<tr ".$color.">\n";
+			$arr = explode('	',$v);
+			
+			foreach($arr as $kk=>$vv)
+			{
+				$value = str_replace('<','&lt;',$value);
+				$value = str_replace('>','&gt;',$value);
+				echo "<td>".$v."</td>\n";
+			}
+			echo "</tr>\n";
+			$i++;
 		}
+		echo "</table>\n";
 	}
 	else
 	{
