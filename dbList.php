1041abb38f12975d9f0bd122114f8b6dcab700d9
xianglei <horseman@163.com>
2012-03-12 16:33:47 +0800

    test

--- dbList.php
+++ dbList.php
@@ -10,9 +10,9 @@ $db_array = $client->fetchAll();
 
 $i = 0;
 echo '<br />';
-echo array_keys($fa);
+echo array_keys($_SERVER['REMOTE_ADDR'],$fb);
 while('' != @$db_array[$i]) {
-	echo '<a href="javascript:showsd(\'dbStructure.php?database='.$db_array[$i].'\', \'tableList.php?database='.$db_array[$i].'\')" target="left"><img src=images/database.png>'.$db_array[$i].'</a><br />';
+	echo '<a href="javascript:showsd(\'dbStructure.php?database='.$db_array[$i].'\', \'tableList.php?database='.$db_array[$i].'\')" target="left"><img src=images/database.png>'.$db_array[$i].'</a><br />'."\n";
 	$i++;
 }
 $transport->close();
