fb6acde164e821533048febad73d39917cc96b1f
xianglei <horseman@163.com>
2012-03-12 16:16:29 +0800

    test

--- dbList.php
+++ dbList.php
@@ -10,6 +10,7 @@ $db_array = $client->fetchAll();
 
 $i = 0;
 echo '<br />';
+echo array_keys($fa);
 while('' != @$db_array[$i]) {
 	echo '<a href="javascript:showsd(\'dbStructure.php?database='.$db_array[$i].'\', \'tableList.php?database='.$db_array[$i].'\')" target="left"><img src=images/database.png>'.$db_array[$i].'</a><br />';
 	$i++;
