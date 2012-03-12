3c983016f87c995d5b402efb3c3ab331d5f2f9a7
xianglei <horseman@163.com>
2012-03-12 16:43:33 +0800

    a

--- dbList.php
+++ dbList.php
@@ -2,6 +2,22 @@
 include_once 'config.inc.php';
 include_once 'templates/style.css';
 
+function multidimensional_search($parents, $searched) {
+	if (empty($searched) || empty($parents)) {
+		return false;
+	}
+
+	foreach ($parents as $key => $value) {
+		$exists = true;
+		foreach ($searched as $skey => $svalue) {
+			$exists = ($exists && IsSet($parents[$key][$skey]) && $parents[$key][$skey] == $svalue);
+		}
+		if($exists){ return $key; }
+	}
+
+	return false;
+}
+
 $transport->open();
 
 $client->execute('show databases');
@@ -11,7 +27,7 @@ $db_array = $client->fetchAll();
 $i = 0;
 echo '<br />';
 $a = array("mobile" => array("ip" => "1","only" => "2"));
-echo array_search("2",$a);
+echo multidimensional_search("2",$a);
 while('' != @$db_array[$i]) {
 	echo '<a href="javascript:showsd(\'dbStructure.php?database='.$db_array[$i].'\', \'tableList.php?database='.$db_array[$i].'\')" target="left"><img src=images/database.png>'.$db_array[$i].'</a><br />'."\n";
 	$i++;
