5066dff8480289b6498f9fd13fb76ca4f48477a0
xianglei <horseman@163.com>
2012-03-12 16:51:28 +0800

    test

--- dbList.php
+++ dbList.php
@@ -2,21 +2,8 @@
 include_once 'config.inc.php';
 include_once 'templates/style.css';
 
-function multidimensional_search($parents, $searched) {
-	if (empty($searched) || empty($parents)) {
-		return false;
-	}
-
-	foreach ($parents as $key => $value) {
-		$exists = true;
-		foreach ($searched as $skey => $svalue) {
-			$exists = ($exists && IsSet($parents[$key][$skey]) && $parents[$key][$skey] == $svalue);
-		}
-		if($exists){ return $key; }
-	}
-
-	return false;
-}
+$ip = $_SERVER['REMOTE_ADDR'];
+$offset = array_search($ip,$fb) + 1;
 
 $transport->open();
 
@@ -26,11 +13,23 @@ $db_array = $client->fetchAll();
 
 $i = 0;
 echo '<br />';
-$a = array("mobile" => array("ip" => "1","only" => "2"));
-echo multidimensional_search($a,"2");
-while('' != @$db_array[$i]) {
-	echo '<a href="javascript:showsd(\'dbStructure.php?database='.$db_array[$i].'\', \'tableList.php?database='.$db_array[$i].'\')" target="left"><img src=images/database.png>'.$db_array[$i].'</a><br />'."\n";
-	$i++;
+if($fb[$offset] == "all")
+{
+	while('' != @$db_array[$i]) {
+		echo '<a href="javascript:showsd(\'dbStructure.php?database='.$db_array[$i].'\', \'tableList.php?database='.$db_array[$i].'\')" target="left"><img src=images/database.png>'.$db_array[$i].'</a><br />'."\n";
+		$i++;
+	}
+}
+else
+{
+	$onlydb = explode(",",$fb[$offset]);
+	while('' != @$db_array[$i]) {
+		if(in_array($db_array[$i],$onlydb))
+		{
+			echo '<a href="javascript:showsd(\'dbStructure.php?database='.$db_array[$i].'\', \'tableList.php?database='.$db_array[$i].'\')" target="left"><img src=images/database.png>'.$db_array[$i].'</a><br />'."\n";
+		}
+		$i++;
+	}
 }
 $transport->close();
 ?>
