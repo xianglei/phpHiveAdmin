b33d0b1018978cf2a0201ad9f4b5b3a378fb1273
xianglei <horseman@163.com>
2012-03-15 12:18:50 +0800

    a

--- config.inc.php
+++ config.inc.php
@@ -3,16 +3,16 @@ session_start();
 $session_id = session_id();
 $session_name = session_name();
 $GLOBALS['THRIFT_ROOT'] = './libs/';
-// load the required files for connecting to Hive
+# load the required files for connecting to Hive
 require_once $GLOBALS['THRIFT_ROOT'] . 'packages/hive_service/ThriftHive.php';
 require_once $GLOBALS['THRIFT_ROOT'] . 'transport/TSocket.php';
 require_once $GLOBALS['THRIFT_ROOT'] . 'protocol/TBinaryProtocol.php';
 require_once $GLOBALS['THRIFT_ROOT'] . 'classes/class.timer.php';
 require_once $GLOBALS['THRIFT_ROOT'] . 'classes/class.auth.php';
 require_once $GLOBALS['THRIFT_ROOT'] . 'packages/ExecutorService/ExecutorService.php';
-// Set up the transport/protocol/client
+# Set up the transport/protocol/client
 require_once 'langs/lang_en.php';
-//langs path en file to use chinese, modify to 'langs/lang_cn.php'
+# langs path en file to use chinese, modify to 'langs/lang_cn.php'
 
 define('HOST','192.168.1.49');
 define('PORT','10000');
@@ -56,10 +56,8 @@ $auth = new Authorize;
 $_SESSION['username'] = @$_GET['username'];
 $_SESSION['password'] = @$_GET['password'];
 
-
 $_SESSION['onlydb'] = $auth->AuthUser("accesslist",$_SESSION['username'],$_SESSION['password']);
 
-echo $_SESSION['onlydb'];
 if($_SESSION['onlydb'] == "")
 {       
         die("Cannot access");
