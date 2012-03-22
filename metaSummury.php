244f2ff126ab5709601e3b8dd5661154b3228f5b
xianglei <horseman@163.com>
2012-03-22 15:04:37 +0800

    1

--- metaSummury.php
+++ metaSummury.php
@@ -7,8 +7,12 @@ $meta = new MysqlMeta();
 $sql = "select count(*) as count from DBS where NAME != 'default'";
 $arr = $meta->GetResult($sql);
 echo "Databases: ".$arr[0]['count'] . "<br>\n";
-unset($arr);
 
 $sql = "select count(*) as count from TBLS";
+$arr = $meta->GetResult($sql);
 echo "Tables: ".$arr[0]['count'] . "<br>\n";
+
+$sql = "select count(*) as count from COLUMNS";
+$arr = $meta->GetResult($sql);
+echo "Columns: ".$arr[0]['count'] . "<br>\n";
 ?>
