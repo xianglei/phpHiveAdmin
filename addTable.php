43fb95f90572490cf060428140051872417798db
xianglei <horseman@163.com>
2012-03-23 13:49:21 +0800

    aa

--- addTable.php
+++ addTable.php
@@ -133,7 +133,7 @@ else
 			else
 			{
 				$ext = '';
-				$tablecomment = " COMMENT".$_POST['tablecomment']." ";
+				$tablecomment = " COMMENT ".$_POST['tablecomment']." ";
 				$partition = '';
 				$limit = '';
 				$stored = " ";
