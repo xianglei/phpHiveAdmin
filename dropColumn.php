92d347e54f79a92f186db4931887edb47008d46b
xianglei <horseman@163.com>
2012-03-26 19:42:06 +0800

    ac

--- dropColumn.php
+++ dropColumn.php
@@ -28,13 +28,25 @@ else
 		while ('' != @$array_desc_table[$i])
 		{
 			$array_desc = explode('	',$array_desc_table[$i]);
-			$array_desc_desc[$i] = $array_desc[0];
-			if($array_desc_desc[$i] == $_GET['column'])
+			$array_desc_desc[$i]['name'] = $array_desc[0];
+			$array_desc_desc[$i]['type'] = $array_desc[1];
+			$array_desc_desc[$i]['comment'] = $array_desc[2];
+			$i++;
+			if($array_desc_desc[$i]['name'] == $_GET['column'])
 			{
 				unset($array_desc_desc[$i]);
 			}
 			$i++;
 		}var_dump($array_desc_desc);
+		#-----------construct sql---------
+		$sql = "ALTER TABLE `".$_GET['database']."`.`".$_GET['table']."` REPLACE COLUMNS ( ";
+		$i = 0;
+		foreach($array_desc_desc as $k => $v):
+			$tmp .= "`".$array_desc_desc[$i]['name']."` ".$array_desc_desc[$i]['type']." COMMENT '".$array_desc_desc[$i]['comment']."',";
+			$i++;
+		endforeach
+		#-----------
+		$sql = $sql.substr($tmp,0,-1).")";echo $sql;
 	}
 }
 
