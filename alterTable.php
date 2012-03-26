8ac818a14c6890e0cc7b8892896e9e7309fbe720
xianglei <horseman@163.com>
2012-03-26 19:21:36 +0800

    a

--- alterTable.php
+++ alterTable.php
@@ -74,6 +74,12 @@ else
 				{
 					//echo '<tr '.$color.'>';
 					//echo '<td>';
+					if($k == 'name')
+					{
+						echo "<td>";
+						echo "<a href=# onclick=\"javascript:realconfirm('".$lang['dropColumnConfirm']."','dropColumn.php?database=".$_GET['database']."&table=".$_GET['table']."&column=".$v."');return false;\"><img src=images/b_drop.png>".$lang['dropTable']."</a>";
+						echo "</td>";
+					}
 					if($k == 'type')
 					{
 						echo "<td>";
@@ -92,12 +98,6 @@ else
 							echo "<option value=".$kk." ".$selected.">".$vv."</option>";
 						}
 						echo "</select>";
-						echo "</td>";
-					}
-					if($k == 'name')
-					{
-						echo "<td>";
-						echo "<a href=# onclick=\"javascript:realconfirm('".$lang['dropColumnConfirm']."','dropColumn.php?database=".$_GET['database']."&table=".$_GET['table']."&column=".$v."');return false;\"><img src=images/b_drop.png>".$lang['dropTable']."</a>";
 						echo "</td>";
 					}
 					if($k == 'name')
---
+++ dropColumn.php
@@ -0,0 +1,38 @@
+<?php
+include_once 'config.inc.php';
+include_once 'templates/style.css';
+
+if(!@$_GET['database'])
+{
+	die($lang['dieDatabaseChoose']);
+}
+else
+{
+	$transport = new TSocket(HOST, PORT);
+	$protocol = new TBinaryProtocol($transport);
+	$client = new ThriftHiveClient($protocol);
+	
+	$transport->open();
+
+	$client->execute('use '.$_POST['database']);
+	
+	if(!@$_GET['table'])
+	{
+		echo "<script>window.location=dbStructure.php?database=".$_GET['database']."</script>";
+	}
+	else
+	{
+		$client->execute('desc '.$_GET['table']);
+		$array_desc_table = $client->fetchAll();
+		$i = 0;
+		while ('' != @$array_desc_table[$i])
+		{
+			$array_desc = explode('	',$array_desc_table[$i]);
+			$array_desc_desc[$i] = $array_desc[0];
+			$i++;
+		}var_dump($array_desc_desc);
+	}
+}
+
+$transport->close();
+?>
