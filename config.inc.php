3c0968429ca2c2d14e3026f9f2f1c5e834c21293
xianglei <horseman@163.com>
2012-03-30 20:40:27 +0800

    ad

--- config.inc.php
+++ config.inc.php
@@ -12,6 +12,7 @@ require_once $GLOBALS['THRIFT_ROOT'] . 'classes/class.hive.php';
 $auth = new Authorize;
 
 # for my company use only below, you can change it to standard authrize as you wish
+# 仅仅是我公司内部使用所需要的验证，你可以自己改造成自己的标准验证方式
 if($_GET['username'] != "" && $_GET['password'] != "")
 {
 	$_SESSION['username'] = $_GET['username'];
@@ -30,6 +31,7 @@ if(($_SESSION['onlydb'] == "") && ($_GET['username'] == "" || $_GET['password'] 
 	die('No access');
 }
 #comment up if you didn't wanna use authenticate
+#如果不需要验证，请注释掉以上部分
 
 # load the required files for connecting to Hive
 require_once $GLOBALS['THRIFT_ROOT'] . 'packages/hive_service/ThriftHive.php';
@@ -37,38 +39,55 @@ require_once $GLOBALS['THRIFT_ROOT'] . 'transport/TSocket.php';
 require_once $GLOBALS['THRIFT_ROOT'] . 'protocol/TBinaryProtocol.php';
 require_once $GLOBALS['THRIFT_ROOT'] . 'classes/class.timer.php';
 # Set up the transport/protocol/client
+
+
+#-----------defination of language--------------
+#-----------定义所使用语言----------------------
 require_once 'langs/lang_en.php';
 # langs path en file to use chinese, modify to 'langs/lang_cn.php'
+# 语言包所使用的路径，中文请使用'lang/lang_cn.php'
 
+
+#-----------defination of HIVE Server and port-----
+#-----------定义Hive Server连接地址与端口----------
 define('HOST','192.168.1.49');
 define('PORT','10000');
+#--------------------------------------------------
+#----------defination of meta type and connection variables-------
+#----------定义元数据管理的类型相关变量---------------------------
 define('METATYPE', 'mysql');
-#can set to mysql pgsql derby, derby may need unixODBC to connect;
+# METATYPE can set to mysql pgsql derby, derby may need unixODBC of php to connect;
+# METATYPE可以设置为mysql,pgsql,derby,但是derby可能会需要php的unixODBC去连接
+
+#----------------
 define('METADB','192.168.1.28');
 define('METAPORT', '3306');
 define('METAUSER', 'hive');
 define('METAPASS', 'hive');
 define('METANAME', 'hive');
-
+#----------------
 #define('METATYPE', 'pgsql');
 #define('METADB','192.168.1.28');
 #define('METAPORT', '5432');
 #define('METAUSER', 'hive');
 #define('METAPASS', 'hive');
 #define('METANAME', 'hive');
-
+#------------------------------------------------------------------
 #------------------
 $env['hive_jar'] = '/opt/modules/hive/hive-0.7.1/lib/hive-contrib-0.7.1.jar';
-#------------------server env
+#------------------server env----------------------
+#------------------定义环境变量--------------------
 $env['hadoop_home'] = '/opt/modules/hadoop/hadoop-0.20.203.0';# hadoop root path
 $env['hive_home'] = '/opt/modules/hive/hive-0.7.1';# hive root path
 $env['java_home'] = '/usr/java/jdk1.6.0_21';# jdk root path
-
+#-----------------------------------------------------------
 $env['etl'] = './etl/';# path to etl configuration files
-#------------------
+#------------------definations of log path and results path, give these path 0777 chmod------------
+#------------------定义日志和结果输出的文件路径，请赋予0777权限------------------------------------
 $env['output_path'] = '/data2/tmp/phpHiveAdmin';# For cliQuery.php, where to put stderr output log file and original result file
 $env['logs_path'] = './logs/';
-#------------------
+#--------------------------------------------------------------------------------------------------
+
 $env['showTables'] = 'show tables';
 $env['bodyColor'] = '#EFEFEF';
 $env['trColor1'] = '#AFAFAF';
