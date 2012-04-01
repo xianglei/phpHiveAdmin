35ccb276db80cd00db41e7464a07483509b9357c
xianglei <horseman@163.com>
2012-04-01 20:09:55 +0800

    a

--- sqlQuery.php
+++ sqlQuery.php
@@ -105,7 +105,7 @@ else
 			</script>
 			";
 			
-			echo "<body bgcolor=#EFEFEF onload=\"ajaxRequest('cliQuery.php?time=".$sha1."&query=".urlencode($sql,$key)."' , getReult)\">";
+			echo "<body bgcolor=#EFEFEF onload=\"ajaxRequest('cliQuery.php?time=".$sha1."&query=".urlencode($sql)."' , getReult)\">";
 			echo "<input type=button value=\"".$lang['getResult']."\" onclick=\"window.open('getResult.php?str=".$sha1."')\">";
 			echo "<br><br>".$lang['fingerprintOfMapReduce']." ".$sha1;
 			echo "<br><br>";
