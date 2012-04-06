adf0e29c814b9e2b12fe856964c7aafda8c04148
xianglei <horseman@163.com>
2012-04-06 13:31:09 +0800

    a

--- history.php
+++ history.php
@@ -45,7 +45,7 @@ if ($dh = opendir($dir))
 			else
 			{
 				$tmp = explode("_", $file);
-				echo $str = $tmp[1]."_".$tmp[2];
+				echo $str = substr($tmp[1]."_".$tmp[2],0,-4);
 				if(file_exists($env['output_path']."/hive_res.".$str.".out"))
 				{
 					echo "<td><a href=getResult.php?str=".$str.">$file</a></td>\n";
