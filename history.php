907524ad500c23813caba9d3650f7d2c9f617897
xianglei <horseman@163.com>
2012-04-06 13:41:45 +0800

    a

--- history.php
+++ history.php
@@ -14,6 +14,9 @@ else
 if ($dh = opendir($dir))
 {
 	echo "<a href=history.php?dir=".$env['logs_path'].">Back to Root</a><br><br>";
+	
+	include_once "templates/search_history.html";
+	
 	echo "<table border=1 cellspacing=1 cellpadding=3>";
 	echo "<tr bgcolor=\"#FFFF99\">";
 	echo "<td>".$lang['filename']."</td><td>".$lang['fileContent']."</td><td>".$lang['filetype']."</td><td>".$lang['filesize']."</td>";
@@ -60,7 +63,7 @@ if ($dh = opendir($dir))
 				while(!feof($fp)):
 					$str .= fgets($fp,1024);
 				endwhile;
-				echo $str;
+				//echo $str;
 				unset ($str);
 				echo "</td>";
 				
