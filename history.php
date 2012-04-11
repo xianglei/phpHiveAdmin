38ee80a4e06b402d4afb82d34594db6d261145bc
xianglei <horseman@163.com>
2012-04-11 19:14:02 +0800

    a

--- history.php
+++ history.php
@@ -35,7 +35,7 @@ if ($dh = opendir($dir))
 	}
 	closedir($dh);
 	
-	$records_per_page = 10;
+	$records_per_page = 20;
 	$pagination = new Zebra_Pagination();
 	$pagination->records(count($file_array));
 	$pagination->records_per_page($records_per_page);
