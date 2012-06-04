b0a795dfcd2d33be9fc0dd92d97157a90a553724
xianglei <horseman@163.com>
2012-06-04 19:52:38 +0800

    finale fileBrowser.php

--- fileBrowser.php
+++ fileBrowser.php
@@ -4,66 +4,6 @@ include_once 'config.inc.php';
 include_once 'templates/style.css';
 
 
-	//echo str_replace("/","\/",$_GET['dir']);
-/*if(preg_match('/'.str_replace("/","\/",$_GET['dir']).'/i',$env['hdfsToHiveDir']) == '0')
-{
-	$dir = $_GET['dir'];
-}
-else
-{
-	$dir = $env['hdfsToHiveDir'];
-}
-
-
-//if (is_dir($dir)) {
-
-if ($dh = opendir($dir))
-{
-	echo "<a href=fileBrowser.php?dir=".$env['hdfsToHiveDir'].">Back to Root</a><br><br>";
-	echo "<table border=1 cellspacing=1 cellpadding=3>";
-	echo "<tr bgcolor=\"#FFFF99\">";
-	echo "<td>".$lang['filename']."</td><td>".$lang['filetype']."</td><td>".$lang['filesize']."</td>";
-	echo "</tr>";
-	$i = 0;
-	while (($file = readdir($dh)) !== false)
-	{
-		if(($i % 2) == 0)
-		{
-			$color = $env['trColor1'];
-		}
-		else
-		{
-			$color = $env['trColor2'];
-		}
-		echo "<tr bgcolor=\"".$color."\">";
-		if(($file == '.') || ($file == '..'))
-		{
-			continue;
-		}
-		else
-		{
-			if(is_dir($dir.$file))
-			{
-				echo "<td><a href=fileBrowser.php?dir=".$dir.$file."/>$file</a></td>\n";
-			}
-			else
-			{
-				echo "<td><a href=fileBrowser.php?dir=".$dir.$file.">$file</a></td>\n";
-			}
-			echo "<td>".filetype($dir.$file)."</td>\n";
-			echo "<td>".filesize($dir.$file)."</td>\n";
-		}
-		echo "</tr>";
-		$i++;
-	}
-	closedir($dh);
-	echo "</table>";
-	echo "Files: ".$i;
-}
-
-//}
-*/
-
 if(!@$_GET['path'])
 {
 	$path = "/";
@@ -113,14 +53,12 @@ foreach( $list_arr as $k => $v)
 		echo "<tr bgcolor=\"".$color."\">";
 		$tmp = explode(" ", $v);
 		$tmp = $etc->ArrayFilter($tmp);
-		var_dump($tmp);
-		#$pos = strpos($v,"/"); 
-		#$str = trim(substr($v,($pos-1),strlen($v)));
+		#var_dump($tmp);
 		echo "<td>".$tmp[0]."</td>";
-		echo "<td>".$tmp[4]."</td>";
-		echo "<td>".$tmp[5]."</td>";
-		echo "<td>".$tmp[16]." ".$tmp[17]."</td>";
-		echo "<td><a href=fileBrowser.php?path=".$tmp[18].">".$tmp[18]."</a></td>\n";
+		echo "<td>".$tmp[2]."</td>";
+		echo "<td>".$tmp[3]."</td>";
+		echo "<td>".$tmp[4]." ".$tmp[5]."</td>";
+		echo "<td><a href=fileBrowser.php?path=".$tmp[6].">".$tmp[6]."</a></td>\n";
 		echo "</tr>";
 	}
 	$i++;
