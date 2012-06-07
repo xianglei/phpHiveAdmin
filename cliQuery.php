9f1d269cc7d9b2ca8b40471967d7a3e87740b5ca
xianglei <horseman@163.com>
2012-06-07 16:32:30 +0800

    urlencode to rawurlencode test

--- cliQuery.php
+++ cliQuery.php
@@ -16,7 +16,7 @@ else
 {
 	if(file_exists($env['output_path']))
 	{
-		$sql = trim(urldecode($query));
+		$sql = trim(rawurldecode($query));
 		$sql = str_replace("\"","'",$sql);
 		$sql = '"'.str_replace('`',"",$sql).'"';
 		
