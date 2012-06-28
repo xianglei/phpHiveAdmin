f42b34d0a23c0e9c12234fe2b7ac958b6bdeb397
xianglei <horseman@163.com>
2012-06-28 13:06:24 +0800

    commit

---
+++ addPartitions.php
@@ -0,0 +1,106 @@
+<?php
+include_once 'config.inc.php';
+include_once 'templates/style.css';
+
+if(!@$_POST['database'])
+{
+	die($lang['dieDatabaseChoose']);
+}
+else
+{
+	$transport = new TSocket(HOST, PORT);
+	$protocol = new TBinaryProtocol($transport);
+	$client = new ThriftHiveClient($protocol);
+	
+	$transport->open();
+
+	$client->execute('use '.$_POST['database']);
+	
+	if(!@$_POST['table'])
+	{
+		echo "<script>window.location=dbStructure.php?database=".$_POST['database']."</script>";
+	}
+	else
+	{
+		echo "<a href=dbStructure.php?database=".$_POST['database'].">".$lang['back']."</a><br><br>";
+		if(!@$_POST['fieldnums'])
+		{
+			echo "<form method=post>";
+			echo $lang['fieldNums']."<input type=text name=fieldnums>";
+			echo "<input type=hidden name=database value=".$_POST["database"].">";
+			echo "<input type=hidden name=table value=".$_POST["table"].">";
+			echo "<input type=submit name=submit value=".$lang['submit'].">";
+			echo "</form>";
+		}
+		else
+		{
+			if(!@$_POST['field_name'] || !@$_POST['field_type'])
+			{
+				echo "<form name=newTable method=post>";
+				echo "<table border=1 cellspacing=1 cellpadding=3>";
+				echo "<tr bgcolor=\"#FFFF99\">
+						<td>".$lang['fieldName']."</td>
+						<td>".$lang['fieldType']."</td>
+						<td>".$lang['comment']."</td>
+					</tr>";
+				$type = array('string'=>'STRING','int'=>'INT');
+				for ($i = 0; $i < $_POST['fieldnums']; $i++)
+				{
+					if(($i % 2) == 0)
+					{
+						$color = $env['trColor1'];
+					}
+					else
+					{
+						$color = $env['trColor2'];
+					}
+					echo "<tr bgcolor=".$color.">\n";
+					//-------------
+					echo "<td>\n";
+					echo "<input type=text name=field_name[]>\n";
+					echo "</td>\n";
+					//-------------
+					echo "<td>\n";
+					echo "<select name=field_type[]>";
+					foreach($type as $kk => $vv)
+					{
+						echo "<option value=".$kk.">".$vv."</option>";
+					}
+					echo "</select>";
+					echo "</td>\n";
+					//-------------
+					echo "<td>\n";
+					echo "<input type=text name=comment[]>\n";
+					echo "</td>\n";
+					//-------------
+					echo "</tr>\n";
+				}
+				echo "<input type=hidden name=database value=".$_POST['database'].">";
+				echo "<input type=hidden name=table value=".$_POST['table'].">";
+				echo "<input type=hidden name=fieldnums value=".$_POST['fieldnums'].">";
+				echo "</table><br>";
+				echo "<input type=submit name=submit value=".$lang['submit'].">";
+				echo "&nbsp;&nbsp;";
+				echo "<input type=button value=".$lang['cancel']." onclick=\"javascript:window.location=dbStructure.php?database=".$_POST['database']."\">";
+				echo "</form>";
+			}
+			else
+			{
+				$sql = "ALTER TABLE ".$_POST['table']." ADD PARTITION (";
+				$i = 0;
+				$str = "";
+				while ("" != @$_POST['field_name'][$i])
+				{
+					$str .= $_POST['field_name'][$i]." ".$_POST['field_type'][$i]." COMMENT '".$_POST['comment'][$i]."',";
+					$i++;
+				}
+				$str = substr($str,0,-1);
+				$sql = $sql.$str.")";
+				echo $sql."<br>";
+				$client->execute($sql);
+				echo "<script>alert('".$lang['alterTableSuccess']."');window.location='dbStructure.php?database=".$_POST['database']."';</script>";
+			}	
+		}
+	}
+	$transport->close();
+}
