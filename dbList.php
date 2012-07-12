e6073ca69fb49bec3619c00dfad92af419549c01
xianglei <horseman@163.com>
2012-07-12 18:17:34 +0800

    debug

--- dbList.php
+++ dbList.php
@@ -17,7 +17,7 @@ echo '<br />';
 if($_SESSION['onlydb'] == "all")
 {
 	while('' != @$db_array[$i]) {
-		echo '<a href="javascript:showsd(\'dbStructure.php?database='.$db_array[$i].'\', \'tableList.php?database='.$db_array[$i].'\')" target="left"><img src=images/database.png>'.$db_array[$i].'</a><br />'."\n";
+		echo '<a href="javascript:showsd(\'tableList.php?database='.$db_array[$i].'\',\'dbStructure.php?database='.$db_array[$i].'\')" target="left"><img src=images/database.png>'.$db_array[$i].'</a><br />'."\n";
 		$i++;
 	}
 }
@@ -27,7 +27,7 @@ else
 	while('' != @$db_array[$i]) {
 		if(in_array($db_array[$i],$onlydb))
 		{
-			echo '<a href="javascript:showsd(\'dbStructure.php?database='.$db_array[$i].'\', \'tableList.php?database='.$db_array[$i].'\')" target="left"><img src=images/database.png>'.$db_array[$i].'</a><br />'."\n";
+			echo '<a href="javascript:showsd(\'tableList.php?database='.$db_array[$i].'\', \'dbStructure.php?database='.$db_array[$i].'\')" target="left"><img src=images/database.png>'.$db_array[$i].'</a><br />'."\n";
 		}
 		$i++;
 	}
