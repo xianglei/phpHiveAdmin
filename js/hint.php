c97fcd2fe41824fc1f1c527ac415dca2a01109f8
xianglei <horseman@163.com>
2012-07-20 15:06:27 +0800

    columns name hints test

---
+++ js/hint.php
@@ -0,0 +1,45 @@
+<?php
+include_once 'config.inc.php';
+
+if(!$_GET['database'])
+{
+	$file = "hiveudfs.txt";
+	$array = file($file);
+}
+else
+{
+	if(!$_GET['table'])
+	{
+		$file = "hiveudfs.txt";
+		$array = file($file);
+	}
+	else
+	{
+		$transport = new TSocket(HOST, PORT);
+		$protocol = new TBinaryProtocol($transport);
+		$client = new ThriftHiveClient($protocol);
+
+		$transport->open();
+		$client->execute('use '.$_GET['database']);
+		$sql = "desc ".$_GET['table'];
+		$client->execute($sql);
+		$array_desc_table = $client->fetchAll();
+		
+		$i = 0;
+		while ('' != @$array_desc_table[$i])
+		{
+			$array_desc = explode('	',$array_desc_table[$i]);
+			$array_desc_desc[$i] = $array_desc[0];
+			$i++;
+		}
+		
+		$file = "hiveudfs.txt";
+		$array = file($file);
+		$array = array_merge($array,$array_desc_desc);
+	}
+}
+foreach ($array as $key => $value)
+{
+	echo $value."\n\r";
+}
+?>
