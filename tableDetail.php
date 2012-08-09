64494e99b6d7c4e56fb7d44a6f52a319dbbb3a30
xianglei <horseman@163.com>
2012-08-09 18:58:42 +0800

    detail location to hdfs browser

--- tableDetail.php
+++ tableDetail.php
@@ -100,6 +100,19 @@ else
 				$color = "bgcolor=\"".$env['trColor2']."\"";
 			}
 			echo "<tr ".$color.">\n";
+			if($array_desc_desc['name'][$i] == "Location:")
+			{
+				echo "<td>".$array_desc_desc['name'][$i]."</td>";
+				$tmp = explode("/",$array_desc_desc['type'][$i]);
+				$tmp = array_shift($tmp);
+				$tmp = array_shift($tmp);
+				$tmp = array_shift($tmp);
+				foreach($tmp as $v)
+				{
+					$str .="/".$v;
+				}
+				echo "<td><a href=fileBrowser.php?path=".$str.">".$array_desc_desc['type'][$i]."</a></td>";
+			}
 			echo "<td>".$array_desc_desc['name'][$i]."</td>";
 			echo "<td>".$array_desc_desc['type'][$i]."</td>";
 			echo "</tr>";
