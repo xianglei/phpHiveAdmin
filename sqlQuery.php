13b7755660a8cc2caf2a3fd4b04cd8f8de2e0292
xianglei <horseman@163.com>
2012-08-24 15:01:36 +0800

    some button css

--- sqlQuery.php
+++ sqlQuery.php
@@ -186,7 +186,7 @@ else
 			$sql = str_replace("%", "\000", $sql);//encode for like %
 			
 			echo "<body bgcolor=#EFEFEF onload=\"ajaxRequest('cliQuery.php?time=".$sha1."&query=".rawurlencode($sql)."' , getReult)\">";
-			echo "<input type=button value=\"".$lang['getResult']."\" onclick=\"window.open('getResult.php?str=".$sha1."')\">";
+			echo "<input class=\"btn btn-success\" type=button value=\"".$lang['getResult']."\" onclick=\"window.open('getResult.php?str=".$sha1."')\">";
 			echo "<br><br>".$lang['fingerprintOfMapReduce']." ".$sha1;
 			echo "<br><br>";
 			echo "SQL: ".$sql;
