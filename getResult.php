87050e2b3afa8d99adc33ffd49be35a63610ce26
xianglei <horseman@163.com>
2012-09-03 20:29:38 +0800

    test

--- getResult.php
+++ getResult.php
@@ -7,6 +7,8 @@ if(!@$_GET['str'])
 }
 else
 {
+	echo '<div class="container">';
+	echo '<div class="span10">';
 	if(!$_SESSION['onlydb'] || $_SESSION['onlydb'] == "")
 	{
 		die("<script>parent.location='index.php'</script>");
@@ -51,7 +53,7 @@ else
 		{
 			if(filesize($filename) != 0)
 			{
-				echo "<input type=button class=\"btn\" name=download value=\"".$lang['downloadResultFile']."\" onclick=\"window.open('download.php?str=".$str."');\"><br><br>";
+				echo "<input type=button class=\"btn btn-success\" name=download value=\"".$lang['downloadResultFile']."\" onclick=\"window.open('download.php?str=".$str."');\"><br><br>";
 
 				$etc = new Etc;
 
@@ -60,8 +62,8 @@ else
 				$array = $etc->GetResult($filename);
 				$array = explode("\n",substr($array,0,-1));//stop at last return
 				$i = 0;
-				echo "<table border=1 cellspacing=1 cellpadding=3>\n";
-				echo "<tr bgcolor=#FFFF99>";
+				echo "<table class=\"table table-striped table-hover\">\n";
+				echo "<tr class=\"info\">";
 				foreach($array_column as $ka => $va)
 				{
 					echo "<td>";
@@ -71,15 +73,6 @@ else
 				echo "</tr>";
 				foreach($array as $k=>$v)
 				{
-					if(($i % 2) == 0)
-					{
-						$color = "bgcolor=\"".$env['trColor1']."\"";
-					}
-					else
-					{
-						$color = "bgcolor=\"".$env['trColor2']."\"";
-					}
-					#$arr = explode('	',$v);
 					$arr = explode(",",$v);
 					echo "<tr ".$color.">\n";
 					foreach($arr as $kk=>$vv)
@@ -103,5 +96,6 @@ else
 			echo $lang['notReadyYet'];
 		}
 	}
+echo "</div></div>";
 }
 ?>
