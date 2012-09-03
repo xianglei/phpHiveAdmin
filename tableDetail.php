eabed08ff4fc440a06f58f88f9cee5ffbde9dee9
xianglei <horseman@163.com>
2012-09-03 21:18:30 +0800

    debug

--- tableDetail.php
+++ tableDetail.php
@@ -17,6 +17,9 @@ else
 
 	//$client->execute('add jar '.$env['hive_jar']);
 	$client->execute('use '.$_GET['database']);
+	
+	echo '<div class="container">';
+	echo '<div class="span10">';
 
 	if(!@$_GET['table'])
 	{
@@ -49,20 +52,12 @@ else
 			$array_desc_desc['comment'][$i] = trim($array_desc[2]);
 			$i++;
 		}
-		echo "<table border=1 cellspacing=1 cellpadding=3>";
-		echo "<tr bgcolor=#FFFF99><td> ".$lang['columnName']." </td><td> ".$lang['columnType']." </td><td> ".$lang['comment']." </td></tr>";
+		echo "<table class=\"table table-condensed table-bordered table-striped table-hover\">";
+		echo "<tr class=info><td> ".$lang['columnName']." </td><td> ".$lang['columnType']." </td><td> ".$lang['comment']." </td></tr>";
 		$i = 0;
 		foreach ($array_desc_table_1 as $k => $v)
 		{
-			if(($i % 2) == 0)
-			{
-				$color = "bgcolor=\"".$env['trColor1']."\"";
-			}
-			else
-			{
-				$color = "bgcolor=\"".$env['trColor2']."\"";
-			}
-			echo "<tr ".$color.">\n";
+			echo "<tr>\n";
 			echo "<td>".$array_desc_desc['name'][$i]."</td>";
 			echo "<td>".$array_desc_desc['type'][$i]."</td>";
 			echo "<td>".$array_desc_desc['comment'][$i]."</td>";
@@ -86,20 +81,12 @@ else
 			$i++;
 		}
 		
-		echo "<table border=1 cellspacing=1 cellpadding=3>";
-		echo "<tr bgcolor=#FFFF99><td> ".$lang['detailedName']." </td><td> ".$lang['detailedName']." </td></tr>";
+		echo "<table class=\"table table-condensed table-bordered table-striped table-hover\">";
+		echo "<tr class=info><td> ".$lang['detailedName']." </td><td> ".$lang['detailedName']." </td></tr>";
 		$i = 0;
 		foreach ($array_desc_table_2 as $k => $v)
 		{
-			if(($i % 2) == 0)
-			{
-				$color = "bgcolor=\"".$env['trColor1']."\"";
-			}
-			else
-			{
-				$color = "bgcolor=\"".$env['trColor2']."\"";
-			}
-			echo "<tr ".$color.">\n";
+			echo "<tr>\n";
 			if($array_desc_desc['name'][$i] == "Location:")
 			{
 				echo "<td>".$array_desc_desc['name'][$i]."</td>";
@@ -134,20 +121,12 @@ else
 			$i++;
 		}
 		
-		echo "<table border=1 cellspacing=1 cellpadding=3>";
-		echo "<tr bgcolor=#FFFF99><td> ".$lang['storageName']." </td><td> ".$lang['storageName']." </td></tr>";
+		echo "<table class=\"table table-condensed table-bordered table-striped table-hover\">";
+		echo "<tr class=info><td> ".$lang['storageName']." </td><td> ".$lang['storageName']." </td></tr>";
 		$i = 0;
 		foreach ($array_desc_table_3 as $k => $v)
 		{
-			if(($i % 2) == 0)
-			{
-				$color = "bgcolor=\"".$env['trColor1']."\"";
-			}
-			else
-			{
-				$color = "bgcolor=\"".$env['trColor2']."\"";
-			}
-			echo "<tr ".$color.">\n";
+			echo "<tr>\n";
 			echo "<td>".$array_desc_desc['name'][$i]."</td>";
 			echo "<td>".$array_desc_desc['type'][$i]."</td>";
 			echo "</tr>";
@@ -172,20 +151,12 @@ else
 				$i++;
 			}
 		
-			echo "<table border=1 cellspacing=1 cellpadding=3>";
-			echo "<tr bgcolor=#FFFF99><td> ".$lang['partitionName']." </td><td> ".$lang['partitionName']." </td><td> ".$lang['comment']." </td></tr>";
+			echo "<table class=\"table table-condensed table-bordered table-striped table-hover\">";
+			echo "<tr class=info><td> ".$lang['partitionName']." </td><td> ".$lang['partitionName']." </td><td> ".$lang['comment']." </td></tr>";
 			$i = 0;
 			foreach ($array_desc_table_4 as $k => $v)
 			{
-				if(($i % 2) == 0)
-				{
-					$color = "bgcolor=\"".$env['trColor1']."\"";
-				}
-				else
-				{
-					$color = "bgcolor=\"".$env['trColor2']."\"";
-				}
-				echo "<tr ".$color.">\n";
+				echo "<tr>\n";
 				echo "<td>".$array_desc_desc['name'][$i]."</td>";
 				echo "<td>".$array_desc_desc['type'][$i]."</td>";
 				echo "<td>".$array_desc_desc['comment'][$i]."</td>";
@@ -198,5 +169,7 @@ else
 		echo "<a href=javascript:history.back()>".$lang['back']."</a><br><br>";
 		
 	}
+	echo "</div>";
+	echo "</div>";
 }
 ?>
