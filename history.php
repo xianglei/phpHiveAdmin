81273f1aff183d40fadb6369b254ba313189eb73
xianglei <horseman@163.com>
2012-09-03 20:33:51 +0800

    test

--- history.php
+++ history.php
@@ -15,7 +15,7 @@ else
 
 if ($dh = opendir($dir))
 {
-	echo "<a href=history.php?dir=".$env['logs_path'].">Back to Root</a><br><br>";
+	echo "<a href=history.php?dir=".$env['logs_path']."><i class=\"icon-backward\"></i> Back to Root</a><br><br>";
 
 	include_once "templates/search_history.html";
 
@@ -68,22 +68,14 @@ if ($dh = opendir($dir))
     $records_per_page
 	);
 	
-	echo "<table border=1 cellspacing=1 cellpadding=3>";
-	echo "<tr bgcolor=\"#FFFF99\">";
+	echo "<table class=\"table table-bordered table-hover table-striped\">";
+	echo "<tr class=\"info\">";
 	echo "<td>".$lang['filename']."</td><td>".$lang['fileContent']."</td><td>".$lang['filetype']."</td><td>".$lang['filesize']."</td>";
 	echo "</tr>";
 	
 	foreach (@$file_array as $index => $file)
 	{
-		if(($index % 2) == 0)
-		{
-			$color = $env['trColor1'];
-		}
-		else
-		{
-			$color = $env['trColor2'];
-		}
-		echo "<tr bgcolor=\"".$color."\">";
+		echo "<tr>";
 
 		if(($file == '.') || ($file == '..'))
 		{
