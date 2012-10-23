6e4c36a20f57ac87705b3190be849a9284aa6680
xianglei <horseman@163.com>
2012-10-23 14:27:58 +0800

    use file to replace cli

--- cliQuery.php
+++ cliQuery.php
@@ -27,11 +27,13 @@ else
 		
 		$sql = str_replace("\000","%",$sql);//decode for like "%"
 		
-		$sql = str_replace('"','\"',$sql);
-		$sql = '"'.str_replace('`',"",$sql).'"';
+		#$sql = str_replace('"','\"',$sql);
+		#$sql = '"'.str_replace('`',"",$sql).'"';
+		$sql = str_replace('`',"",$sql);
 		
 		#log sql action
-		$logfile = $env['logs_path'].$_SESSION['username']."_".$time.".log";
+		$basedir = dirname(__FILE__);
+		$logfile = $basedir.$env['logs_path'].$_SESSION['username']."_".$time.".log";
 		$etc->LogAction($logfile,"w",$sql."\n");
 		#
 		
@@ -39,7 +41,8 @@ else
 		if(!file_exists($env['output_path'].'/hive_res.'.$time.'.out') || filesize($env['output_path'].'/hive_res.'.$time.'.out') == 0)
 		{
 			
-			$exec = $LANG . $HADOOP_HOME . $HIVE_HOME . $JAVA_HOME. $env['hive_home'].'/bin/hive '.$UDF.' -e '.$sql.' > '.$env['output_path'].'/hive_res.'.$time.'.out';
+			#$exec = $LANG . $HADOOP_HOME . $HIVE_HOME . $JAVA_HOME. $env['hive_home'].'/bin/hive '.$UDF.' -e '.$sql.' > '.$env['output_path'].'/hive_res.'.$time.'.out';
+			$exec = $LANG . $HADOOP_HOME . $HIVE_HOME . $JAVA_HOME. $env['hive_home'].'/bin/hive '.$UDF.' -f '.$logfile.' > '.$env['output_path'].'/hive_res.'.$time.'.out';
 			
 			//passthru($exec);
 			#$log = $env['logs_path'].$time.".debug";
@@ -61,11 +64,15 @@ else
 		
 		$sql = str_replace("\000","%",$sql);//decode for like "%"
 		
-		$sql = str_replace("\"","'",$sql);
-		$sql = '"'.str_replace('`',"",$sql).'"';
+		#$sql = str_replace('"','\"',$sql);
+		#$sql = '"'.str_replace('`',"",$sql).'"';
+		$sql = str_replace('`',"",$sql);
 		
 		#log sql action
-		$logfile = $env['logs_path'].$_SESSION['username']."_".$time.".log";
+		$basedir = dirname(__FILE__);
+		
+		#log sql action
+		$logfile = $basedir.$env['logs_path'].$_SESSION['username']."_".$time.".log";
 		$etc->LogAction($logfile,"w",$sql."\n");
 		#
 		
@@ -76,7 +83,8 @@ else
 			$HIVE_HOME = 'export HIVE_HOME='.$env['hive_home'].'; ';
 			$JAVA_HOME = 'export JAVA_HOME='.$env['java_home'].'; ';
 
-			$exec = $LANG . $HADOOP_HOME . $HIVE_HOME . $JAVA_HOME. $env['hive_home'].'/bin/hive '.$UDF.' -e '.$sql.' > '.$env['output_path'].'/hive_res.'.$time.'.out';
+			#$exec = $LANG . $HADOOP_HOME . $HIVE_HOME . $JAVA_HOME. $env['hive_home'].'/bin/hive '.$UDF.' -e '.$sql.' > '.$env['output_path'].'/hive_res.'.$time.'.out';
+			$exec = $LANG . $HADOOP_HOME . $HIVE_HOME . $JAVA_HOME. $env['hive_home'].'/bin/hive '.$UDF.' -f '.$logfile.' > '.$env['output_path'].'/hive_res.'.$time.'.out';
 			
 			//passthru($exec);
 			$runfile = $env['output_path']."/hive_run.".$time.".out";
