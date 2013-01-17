88ccce03ec04c289c46dba7d00128117a4e71295
root <root@localhost.(none)>
2013-01-17 23:50:17 +0800

    i

--- install_centos6.sh
+++ install_centos6.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 yum install -y php php-cli php-devel php-common httpd httpd-devel php-mbstring php-mysql php-pdo php-process mysql mysql-devel mysql-server mysql-libs wget lrzsz dos2unix pexpect libxml2 libxml2-devel
 service mysqld start
-mysql -hlocalhost -uroot easyhadoop < easyhadoop.sql
+mysql -e "create database if not exists easyhadoop;"
+mysql -hlocalhost -uroot easyhadoop < phpHiveAdmin.sql
 mkdir -p /var/www/html/phpHiveAdmin
 cp -R * /var/www/html/phpHiveAdmin/
 mkdir -p /var/www/html/phpHiveAdmin/logs
